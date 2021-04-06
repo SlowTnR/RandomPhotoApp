2e7522c87698cbc563fee985d58e06e490b162ec
Ilja Patrushev <slowtnr@gmail.com>
2021-04-06 19:51:39 +0300

    Initial Commit

---
+++ RandomPhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhotoApp
+//
+//  Created by Ilja Patrushev on 6.4.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
